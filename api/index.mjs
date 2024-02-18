74ffdaa1a6fe0e4dae62b17dde9f1748d8e3e492
Bablu Choudhury <babluchoudhury478@gmai.com>
2024-02-18 20:42:04 +0530

    first commit

--- api/index.mjs
+++ api/index.mjs
@@ -8,6 +8,12 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors({origin:"*"}))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://movies-frontend-eight.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.get("/",(req,res)=>{
     res.send("Server Live")
 })
